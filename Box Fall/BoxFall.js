6e6795b9282e92d64b84b7c61e74e3346fc20dbc
Ankit <iamankit054@gmail.com>
2022-08-17 20:02:18 +0530

    Add Player Controller Button in Ball Fall Game.

--- Box Fall/BoxFall.js
+++ Box Fall/BoxFall.js
@@ -43,11 +43,6 @@ function Gameplay()
             if(boxes[i].Collision(player)) 
             {
                 clearInterval(gameInterval);
-                // ctx.beginPath();
-                // ctx.fillStyle = 'red';
-                // ctx.font = '100px cursive';
-                // ctx.fillText('Game Over', (width-500)*0.5, height*0.7);
-                // ctx.closePath();
                 alert('Game Over');
                 document.body.appendChild(gameUI);
             }
--- Box Fall/EventHandler.js
+++ Box Fall/EventHandler.js
@@ -15,15 +15,6 @@ window.addEventListener('keyup', function(event)
     horizontalDirection = 0;
 });
 
-window.addEventListener('touchstart', function(event)
-{
-    if(event.touches[0].clientX < this.screen.width/2) {
-        horizontalDirection = -1;
-    }
-    else {
-        horizontalDirection = 1;
-    }
-});
 window.addEventListener('touchend', function(event)
 {
     horizontalDirection = 0;
